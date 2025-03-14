ee401c699afd4bef76fa7f94a9d5ee604885a605
JutsinC <114007570+JutsinC@users.noreply.github.com>
2025-03-14 12:57:28 -0400

    final comments

--- src/renderer/HelpButton.js
+++ src/renderer/HelpButton.js
@@ -2,7 +2,7 @@
 import styles from './styles/HelpInteractive.module.css'
 import { FaQuestion } from "react-icons/fa6";
 
-//function
+//function for the help button
 function HelpButton({setHelp}) {
   return (
     <div className={styles["help-button"]} onClick={() => {console.log("hello"); setHelp(prevHelp => !prevHelp)}}>
